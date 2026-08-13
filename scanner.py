7a5c39a36aa0d77247da7cc324c37485a8d2cdcc
mogoat <m.ishak06@icloud.com>
2026-08-13 10:40:20 +0200

    core logic implemented. Need to reformat code and fix GUI

--- scanner.py
+++ scanner.py
@@ -1,3 +1,5 @@
+# Disclaimer
+#This tool is provided for educational and authorized testing purposes only. The author assumes no liability and is not responsible for any misuse or damage caused by this program.
 import socket
 import sys
 from datetime import datetime
@@ -30,16 +32,50 @@ def scanner(args):
 
 def button_callback():
     print("button clicked")
+    # User input
+    remoteServer = textbox.get("1.0", "end-1c")
+    remoteServerIP = socket.gethostbyname(remoteServer)
+
+    # Print a banner with information on which host we are about to scan
+    print(" ")
+    print("Please wait, scanning remote host", remoteServerIP)
+    print(" ")
+
+    # Check the date and time the scan was started
+    time1 = datetime.now()
+
+    # Create tuples containing both the IP and each port for the worker pool
+    tasks = [(remoteServerIP, port) for port in range(1, 2000)]
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=200) as pool:
+        results = pool.map(scanner, tasks)
+
+    # Checking time again
+    time2 = datetime.now()
+
+    # Calculate the difference in time to know how long the scan took
+    totaltime = time2 - time1
+
+    # Print the Information
+    print("Total time: ", totaltime)
+
 
 
 if __name__ == '__main__':
-    app = customtkinter.CTk()
-    app.geometry("400x150")
 
-    button = customtkinter.CTkButton(app, text="my button", command=button_callback)
+    app = customtkinter.CTk()
+    app.geometry("800x800")
+
+
+    label = customtkinter.CTkLabel(app, text="CTkLabel", fg_color="transparent")#
+    label.pack(padx=20, pady=11)
+    label.configure(text="Port-Scanner")
+    text = label.cget("text")
+    button = customtkinter.CTkButton(app, text="Start_Scan", command=button_callback)
     button.pack(padx=20, pady=20)
     textbox = customtkinter.CTkTextbox(app)
     textbox.pack(padx=20, pady=10, fill="both", expand=True)
     textbox.insert("0.0", "new text to insert")
-    textbox.configure(state="disabled")
+    #textbox.configure(state="enabled")
+
     app.mainloop()
